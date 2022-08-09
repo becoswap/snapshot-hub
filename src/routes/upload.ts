16218fb4f1b187d6f9ad85a3e9f15fd480b5025f
beco <cool@becoswap.com>
2022-08-09 16:23:34 +0700

    update upload ipfs

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,28 +1,23 @@
 import express from 'express';
 import multer from 'multer';
 import fs from 'fs';
-import pinataSDK from '@pinata/sdk';
 import { sendError } from '../helpers/utils';
+import { create, IPFSHTTPClient } from 'ipfs-http-client';
 
 const router = express.Router();
 
 const fileSize = 1000 * 1000;
 const upload = multer({ dest: 'uploads/', limits: { fileSize } });
 
-const pinataConfig: any = {
-  key: process.env.PINATA_API_KEY,
-  secret: process.env.PINATA_SECRET_API_KEY
-};
-
-const pinata = pinataSDK(pinataConfig.key, pinataConfig.secret);
+const ipfsClient: IPFSHTTPClient = create(process.env.IPFS as any);
 
 router.post('/upload', upload.single('file'), async (req, res) => {
   const path = `${req.file.destination}${req.file.filename}`;
   const readableStreamForFile = fs.createReadStream(path);
   try {
-    const result = await pinata.pinFileToIPFS(readableStreamForFile);
+    const result = await ipfsClient.add(readableStreamForFile);
     const file = {
-      ipfs_hash: result.IpfsHash,
+      ipfs_hash: result.cid.toString(),
       mimetype: req.file.mimetype,
       size: req.file.size
     };
